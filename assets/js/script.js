d39b4cdab149e99c0bf92b259b55794009411586
Sebastian Morales <semorcar@gmail.com>
2024-07-19 23:05:34 -0400

    Creada la responsividad de la página y cambiada la fuente

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+
+$("#botonEnviar").click(function(){
+    alert("Mensaje enviado correctamente");
+});
+
+$("#adorno1").hover(function(){
+    $(this).css({ "background-color": "blue", "color": "white" });
+});
+
+$("#adorno2").hover(function(){
+    $(this).css({ "background-color": "blue", "color": "white" });
+});
+
+$("#adorno3").hover(function(){
+    $(this).css({ "background-color": "blue", "color": "white" });
+});
